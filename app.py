aca9a06ded14c0b1baf8e8a7ca3d2fb8e063677e
Cristian Barbosa <Cristianbr7@live.com>
2024-08-13 21:00:19 -0500

    Se agrega pagina de excel

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, jsonify
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 import json
+import pandas as pd
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 @app.route('/')
 def login():
@@ -14,6 +17,22 @@ def dashboard():
 @app.route('/reports')
 def reports():
     return render_template('reports.html')
+
+# Ruta para la subida de archivos
+@app.route('/upload_excel', methods=['GET', 'POST'])
+def upload_excel():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and file.filename.endswith('.xlsx'):
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_path)
+            # Leer el archivo de Excel usando pandas
+            df = pd.read_excel(file_path)
+            # Aquí puedes hacer algo con los datos, como guardarlos en la base de datos
+            return jsonify({'message': 'File uploaded successfully', 'data': df.to_dict()}), 200
+        else:
+            return jsonify({'message': 'Invalid file format. Please upload an Excel file.'}), 400
+    return render_template('upload_excel.html')
 
 @app.route('/logout')
 def logout():
